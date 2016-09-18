10801aac1a15f7ad35e0fa5f6613e0022ed5b890
kpman <s92f002@hotmail.com>
2016-09-18 11:31:23 +0800

    leetcode contest#5 Q2

---
+++ src/__tests__/readBinaryWatch.spec.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+
+import readBinaryWatch from '../readBinaryWatch';
+
+test(`Given a non-negative integer n which represents the number of LEDs that are currently on,
+return all possible times the watch could represent.`, t => {
+  const expected = ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"];
+  t.deepEqual(readBinaryWatch(1).sort(), expected.sort());
+  t.deepEqual(readBinaryWatch(9), []);
+});
---
+++ src/readBinaryWatch.js
@@ -0,0 +1,60 @@
+/**
+ * @param {number} num
+ * @return {string[]}
+ */
+var readBinaryWatch = function(num) {
+  const hoursObj = {};
+  const minsObj = {};
+  const totalObj = {};
+
+  function getBinaryNum(n) {
+    return (n >>> 0).toString(2).split('').filter(i => i === '1').length;
+  }
+
+  function getCombine(a, b) {
+    const result = [];
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < b.length; j++) {
+        const hour = a[i];
+        let min = b[j];
+        if (b[j] < 10) {
+          min = `0${b[j]}`;
+        }
+        const str = `${hour}:${min}`;
+        result.push(str);
+      }
+    }
+    return result;
+  }
+
+  for (let i = 0; i < 12; i++) {
+    if (hoursObj[getBinaryNum(i)] === undefined) {
+      hoursObj[getBinaryNum(i)] = [];
+    }
+    hoursObj[getBinaryNum(i)].push(i);
+  }
+
+  for (let i = 0; i < 60; i++) {
+    if (minsObj[getBinaryNum(i)] === undefined) {
+      minsObj[getBinaryNum(i)] = [];
+    }
+    minsObj[getBinaryNum(i)].push(i);
+  }
+
+  if (num === 0) return ['0:00'];
+
+  for (let i = 0; i < Object.keys(hoursObj).length; i++) {
+    for (let j = 0; j < Object.keys(minsObj).length; j++) {
+      const indexSum = i + j;
+      if (totalObj[indexSum] === undefined) {
+        totalObj[indexSum] = [];
+      }
+      totalObj[indexSum] = totalObj[indexSum].concat(
+        getCombine(hoursObj[Object.keys(hoursObj)[i]], minsObj[Object.keys(minsObj)[j]])
+      );
+    }
+  }
+  return totalObj[num] || [];
+};
+
+export default readBinaryWatch;
